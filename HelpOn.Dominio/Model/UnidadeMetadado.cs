f41bd56a3a9f9067ec0df5f1915ce8be00fbc47b
Henrique Lopes <hlopsouza@hotmail.com>
2016-12-04 21:33:31 -0200

    Mapeando as classes do banco e criando uma partial class para Unidade

---
+++ HelpOn.Dominio/Model/UnidadeMetadado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpOn.Dominio.Model
+{
+
+    [MetadataType(typeof(UnidadeMetadado))]
+    public partial class Unidade
+    {
+
+    }
+    public class UnidadeMetadado
+    {
+        public int IDUnidade { get; set; }
+        public string Nome { get; set; }
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+
+    }
+}
