a6f077e0df89d6a4844725ba1b32d1d09d1696e7
Henrique Lopes <hlopsouza@hotmail.com>
2016-12-08 19:00:16 -0200

    Add funcionalidade de info sobre funcionários e remoção

---
+++ HelpOn/ViewModel/FuncionarioViewModel.cs
@@ -0,0 +1,26 @@
+﻿using HelpOn.Dominio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpOn.Web.ViewModel
+{
+    public class FuncionarioViewModel
+    {
+        public int IDFuncionario { get; set; }
+     
+        public string Nome { get; set; }
+    
+        public string CPF { get; set; }
+       
+        public string Email { get; set; }
+       
+        public string Senha { get; set; }
+        public Nullable<System.DateTime> DataCadastro { get; set; }
+      
+        public int IDNivel { get; set; }
+
+        public virtual Nivel Nivel { get; set; }
+    }
+}
